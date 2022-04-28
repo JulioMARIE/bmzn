b25ee41cf28976fb717867c8bd8fe0e11f312bff
JulioMARIE <julioracechoisie@gmail.com>
2022-04-28 12:42:11 +0100

    Inits

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Bemazone</a>
+      </header>
+      <main>
+        List
+      </main>
+    </div>
+  );
+}
+
+export default App;
